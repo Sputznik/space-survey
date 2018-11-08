8ac1ad44d1e6b143a7a0b13455f7b3cfcc582262
Samuel Thomas <samvthom16@gmail.com>
2018-11-08 18:57:30 +0530

    [#8] - format page description

--- frontend/partials/page.php
+++ frontend/partials/page.php
@@ -1,4 +1,4 @@
-<div class='space-desc'><?php _e( $page->description );?></div>
+<div class='space-title page-desc'><?php _e( $page->description );?></div>
 <div class='space-questions'>
 <?php 
 	foreach( $page->questions as $question ){
