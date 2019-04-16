188b290143224a8b70a22561cafc34f52dad6dd5
unknown <samvthom16@gmail.com>
2019-04-16 15:57:54 +0530

    #14 - by defualt child questions are hidden

--- frontend/partials/question.php
+++ frontend/partials/question.php
@@ -1,16 +1,19 @@
 <?php
-	
+
 	$required_questions = $this->survey->required_questions;
-	
+
 	// FIGURE CLASS FOR THE QUESTION WRAPPER
 	$question_class = 'space-question';
 	if( in_array( $question->ID, $required_questions ) ){
 		$question_class .= ' required';
 		$question->title .= ' *';
 	}
-	
+	else{
+		$question_class .= ' hide';
+	}
+
 	//print_r( $this->survey->rules );
-	
+
 	$question->rules = array();
 	if( is_array( $this->survey->rules ) && isset( $this->survey->rules[ $question->ID ] ) ){
 		foreach( $this->survey->rules[ $question->ID ] as $rule ){
@@ -18,7 +21,7 @@
 			array_push( $question->rules, $rule );
 		}
 	}
-	
+
 ?>
 <div id='<?php _e( 'q'.$question->ID );?>' data-rules='<?php _e( wp_json_encode( $question->rules ) );?>' data-type='<?php _e( $question->type );?>' class='<?php _e( $question_class );?>'>
 	<h5><?php _e( $question->title );?></h5>
