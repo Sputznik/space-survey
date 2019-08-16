0c59e8245299bd8a19e3af61ee24dc7061222722
Steve <anil.stephen66@gmail.com>
2019-08-16 14:53:13 +0530

    Added separate class to checkboxes-with-other

--- frontend/partials/choices-checkbox-other.php
+++ frontend/partials/choices-checkbox-other.php
@@ -7,7 +7,7 @@
 		</label>
 	</li>
 	<?php endforeach;?>
-	<li class='space-choice'>
+	<li class='space-choice-other'>
 		<label style="margin-right: 10px;"><?php _e( 'Other' );?></label>
 		<input type='text' name='<?php _e( $this->get_input_name( $question->ID, 'other' ) );?>' value='' />
 	</li>
