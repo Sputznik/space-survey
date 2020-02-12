88fae04b56274f66cffddf16633eae36d155b2ab
unknown <samvthom16@gmail.com>
2020-02-12 11:35:10 +0530

    #31 - bump css versino

--- space-survey.php
+++ space-survey.php
@@ -13,7 +13,7 @@
 		exit;
 	}
 	//Constant changes all the js and css version on the go
-	define( 'SPACE_SURVEY_VERSION', '2.0.9' );
+	define( 'SPACE_SURVEY_VERSION', '2.1.0' );
 
 	$inc_files = array(
 		'class-space-base.php',
