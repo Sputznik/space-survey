a51473a1999a714ff8388ccac729771bb053e49c
unknown <samvthom16@gmail.com>
2020-02-12 18:14:34 +0530

    #29 - bump css version

--- space-survey.php
+++ space-survey.php
@@ -13,7 +13,7 @@
 		exit;
 	}
 	//Constant changes all the js and css version on the go
-	define( 'SPACE_SURVEY_VERSION', time() ); //2.1.0
+	define( 'SPACE_SURVEY_VERSION', '2.1.1' ); 
 
 	$inc_files = array(
 		'class-space-base.php',
